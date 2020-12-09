795b2e162fb397cbd0223c8bd83c4765f85a0fdd
Oseas <O.a.sanchez@outlook.com>
2020-12-09 15:57:54 -0400

    Loader component completed

---
+++ src/components/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-card outlined tile class="p-2">
+      <v-skeleton-loader
+        class="mx-auto"
+        max-width="300"
+        type="card"
+      ></v-skeleton-loader>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loader",
+};
+</script>
