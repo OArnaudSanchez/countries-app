4d133c1064471981bea61663e15fe29408bde6e5
Oseas <O.a.sanchez@outlook.com>
2020-12-09 16:02:50 -0400

    Regions view completed

---
+++ src/views/Regions.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <Title :title="title" />
+    <v-row no-gutters>
+      <v-col cols="10" sm="2" v-for="(region, index) of Regions" :key="index">
+        <Card :region="region" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+import Title from "../components/Title.vue";
+export default {
+  name: "Regions",
+  components: {
+    Card,
+    Title,
+  },
+  data() {
+    return {
+      title: "Regions",
+      Regions: ["Americas", "Asia", "Europe", "Oceania"],
+    };
+  },
+};
+</script>
