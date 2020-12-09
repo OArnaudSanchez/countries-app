45bc6f8b1c05d24b3013039e5e95b644c40c3f10
Oseas <O.a.sanchez@outlook.com>
2020-12-09 16:00:34 -0400

    App component completed

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NavBar />
+    <router-view></router-view>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
+export default {
+  name: "App",
+
+  components: {
+    NavBar,
+    Footer,
+  }
+};
+</script>
+
+<style >
+html{
+  font-family: 'Lato', sans-serif, 'raleway';
+  background: #f5f5f5;
+}
+.tarjeta {
+  
+  margin: 14px 10px;
+}
+.card-title {
+  padding-bottom: 1px;
+}
+h1 {
+  padding-left: 10px;
+}
+</style>
