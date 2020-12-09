d72e41d15ba0a63f64537a193b46f80252450a43
Oseas <O.a.sanchez@outlook.com>
2020-12-09 15:58:18 -0400

    NavBar app component completed

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-toolbar dark flat>
+      <v-tabs dark height="65">
+        <v-tab class="tab">
+          <router-link
+            class="v-btn v-btn--contained v-btn--router theme--dark v-size--default transparent"
+            :to="{ name: 'Home' }"
+            >Countries App</router-link
+          >
+        </v-tab>
+        <v-tab>
+          <router-link
+            class="item-tab v-btn--active v-btn v-btn--contained v-btn--router theme--dark v-size--default transparent"
+            :to="{ name: 'Countries', params: { code: 'All' } }"
+            >Countries</router-link
+          >
+        </v-tab>
+        <v-tab>
+          <router-link
+            class="item-tab v-btn--active v-btn v-btn--contained v-btn--router theme--dark v-size--default transparent"
+            :to="{ name: 'Regions' }"
+            >Regions</router-link
+          >
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import router from "@/router";
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  margin-left: 42px;
+}
+.item-tab {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
